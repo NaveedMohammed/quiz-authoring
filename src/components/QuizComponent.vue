d70dd2a3add654316ca20ec76cb8c4dc461529fb
NaveedMohammed <rmdnaveed@gmail.com>
2022-08-18 14:46:54 -0500

    Button to add new question

--- src/components/QuizComponent.vue
+++ src/components/QuizComponent.vue
@@ -5,6 +5,7 @@
       <li v-for="(questions, index) in quizQuestions" :key="index">
         {{ questions["question"] }}
       </li>
+      <button type="button" class="btn btn-primary" @click="createQuizQuestion">Add Question</button>
     </ul>
   </div>
 </template>
@@ -16,12 +17,27 @@ export default {
     return {};
   },
   computed: {
+    /**
+     * Gets the list of questiosn from store.
+     */
     quizQuestions() {
       return this.$store.getters.getQuizQuestions;
     },
+    /**
+     * Gets quiz list length from store.
+     */
     quizLength() {
       return this.$store.getters.getQuizLength;
     },
   },
+  methods:{
+    /**
+     * Triggered when new question button is clicked.
+     * Adds a new quiz question to store and in turn to the view
+     */
+    createQuizQuestion(){
+      this.$store.dispatch("addQuizQuestion");
+    }
+  }
 };
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,45 +1,66 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
 export default createStore({
   state: {
     quizQuestions: [
       {
-        "id":0,
-        "question": "What is c2stem?",
-        "image": "",
-        "options": {
-          "a": 1,
-          "b": 2,
-          "c": 3,
-          "d": 4
-        }
+        id: 0,
+        question: "What is c2stem?",
+        image: "",
+        options: {
+          a: 1,
+          b: 2,
+          c: 3,
+          d: 4,
+        },
       },
       {
-        "id":1,
-        "question": "What language is c2stem written in?",
-        "image": "",
-        "options": {
-          "a": 1,
-          "b": 2,
-          "c": 3,
-          "d": 4
-        }
+        id: 1,
+        question: "What language is c2stem written in?",
+        image: "",
+        options: {
+          a: 1,
+          b: 2,
+          c: 3,
+          d: 4,
+        },
       },
     ],
     quizLength: 0,
   },
   getters: {
-    getQuizQuestions(state){
+    getQuizQuestions(state) {
       return state.quizQuestions;
     },
-    getQuizLength(state){
-      return state.quizQuestions.length;
-    }
+    getQuizLength(state) {
+      state.quizLength = state.quizQuestions.length;
+      return state.quizLength;
+    },
   },
   mutations: {
+    /**
+     * Add a new empty question to the store.
+     * @param {state} state 
+     */
+    addQuizQuestion(state) {
+      let newQuestion = {
+        id: state.quizLength,
+        question: "New Question",
+        image: "",
+        options: {
+          a: "",
+          b: "",
+          c: "",
+          d: "",
+        },
+      };
+      state.quizQuestions = [...state.quizQuestions, newQuestion]
+    },
   },
   actions: {
+    addQuizQuestion(context) {
+      context.commit("addQuizQuestion");
+    }
   },
-  modules: {
-  }
-})
+  modules: {},
+});
